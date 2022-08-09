9b620a5b30d453038fe2458b2608ec1d54bd19ed
Felipe Santos <felipe.031293@gmail.com>
2022-08-09 14:14:38 -0300

    adjust server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import App from './app';
 
 const app = new App();
 
-app.init().listen(process.env.PORT, () => {
+app.init().listen(process.env.PORT || 3000, () => {
   console.info(`App starting at http://localhost:${process.env.PORT}`);
   console.info(`Envs: ${process.env.TARGET || 'local'}`);
 });
